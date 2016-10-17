2ae56389b14fd481fd9a7b4ad035c4ed8efe29c4
Ander <ander.uraga@gmail.com>
2016-10-17 10:03:33 +0200

    Initial Commit

---
+++ Mapas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mapas
+//
+//  Created by Spot Matic SL on 17/10/16.
+//  Copyright © 2016 Spot Matic SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
